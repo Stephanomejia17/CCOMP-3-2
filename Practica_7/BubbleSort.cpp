69d12ed95d5fe39ae81a970a91b3c8c414279c31
Walter Stephano Mejia Morales <walter.mejia@ucsp.edu.pe>
2021-11-08 17:51:22 -0500

    Avance

--- Practica_7/BubbleSort.cpp
+++ Practica_7/BubbleSort.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
-int mayorque(int a, int b){
-  if(a>b) return 1;
-  return 0;
-}
-int menorque(int a, int b){
-  if(a<b) return 1;
-  return 0;
-}
-int comparar(int a, int b,int(*p)(int,int)){
-  return p(a,b);
-}
-int absoluto(int a, int b,int(*p)(int,int)){
-  a = abs(a);
-  b = abs(b);
-  return p(a,b);
-}
 
-void BubbleSort(int* A,int n, int(*p)(int,int,int(*)(int,int)),int(pComp)(int,int)){
+class Comparador{
+  public:
+    int a;
+    int b;
+    int mayorque(int a, int b){
+      if(a>b) return 1;
+        return 0;
+    }
+    int menorque(int a, int b){
+      if(a<b) return 1;
+        return 0;
+    }
+    int comparar(int a, int b,Comparador *pO, int(Comparador::*pm)(int,int)){
+      return (pO->*pm)(a,b);
+    }
+    int absoluto(int a, int b,Comparador *pO, int(Comparador::*pm)(int, int)){
+      a = abs(a);
+      b = abs(b);
+      return (pO->*pm)(a,b);
+    }
+};
+
+void BubbleSort(int* A,int n,Comparador *obj ,int(Comparador::*p)(int, int, Comparador *, int(Comparador::*)(int,int)),int(Comparador::*pComp)(int,int)){
   int i,j, temp;
   for(i=0;i<n;i++)
     for(j=0;j<n-1;j++){
-      if(p(A[j],A[j+1],pComp)){
+      if((obj->*p)(A[j],A[j+1],obj,pComp)){
         temp = A[j];
         A[j] = A[j+1];
         A[j+1] = temp;
@@ -28,18 +34,22 @@ void BubbleSort(int* A,int n, int(*p)(int,int,int(*)(int,int)),int(pComp)(int,in
     }
 }
 
-typedef int(*PF1)(int,int,int(*)(int,int));
-typedef int(*PF2)(int,int);
 
+typedef int(Comparador::*PF1)(int, int); //Mayor, menor que
+typedef int(Comparador::*PF2)(int, int, Comparador*, int(Comparador::*)(int,int));
+ 
 int main() {
   const int dim1=4;const int dim2=5;
   int A[]={6,2,-8,3,-5};
   int B[]={3,2,7,1,14};
   int C[]={-5,-2,18,3,4};
   int D[]={10,14,13,-4,1};
-  int* vectores[dim1] ={A,B,C,D}; 
-  PF1 comparadores[2] = {comparar,absoluto};
-  PF2 comparaciones[2] = {mayorque,menorque};
+  int* vectores[dim1] ={A,B,C,D};
+  Comparador* obj;
+
+  PF1 comparadores[2] = {&Comparador::mayorque, &Comparador::menorque};
+  PF2 comparaciones[2] = {&Comparador::comparar, &Comparador::absoluto};
+
   PF1* ppff1 = comparadores;
   PF2* ppff2 = comparaciones;
   int op1[dim1]= {1,0,0,1};
@@ -47,17 +57,16 @@ int main() {
   int* pop1 = op1; 
   int* pop2 = op2;  
   int **pvectores = vectores;
-
   std::cout <<"Vectores Desordenados\n";
   for(int** p=vectores;p<vectores+dim1;p++){
     for(int* q=*p;q<*p+dim2;q++){
       std::cout <<*q<<",";
     }
     std::cout <<"\n";
-  }  
+  } 
   for(int i =0; i <dim1; i++)
   { 
-    BubbleSort(*pvectores,dim2, *(ppff1+*pop1),*(ppff2+*pop2));
+    BubbleSort(*pvectores,dim2, obj,*(ppff2+*pop2), *(ppff1+*pop1));
     pop1++;
     pop2++;
     pvectores++;
@@ -68,5 +77,5 @@ int main() {
       std::cout <<*q<<",";
     }
     std::cout <<"\n";
-  } 
+  }
 }
